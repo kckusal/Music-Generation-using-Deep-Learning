e350f564ffbf21636d118e42e3e126e3b6f09b5b
Kusal KC <kckusal99@gmail.com>
2021-03-06 10:23:23 +0300

    add cors to app & enable on /generate route

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
@@ -10,6 +13,7 @@ def home():
 
 
 @app.route('/generate')
+@cross_origin()
 def generate():
     # generate music with your model & return
     return "Here's a generated music...Enjoy!"
