05aa2edada67bc932fd4d1eb6afa011b33ea7075
Kusal KC <kckusal99@gmail.com>
2021-03-06 09:11:02 +0300

    update homepage message

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'This is an api to get generated music with a model trained using Deep Learning techniques.'
+    return 'Hello! This is an api to get generated music with a model trained using Deep Learning techniques. It was built as part of a project in Deep Learning course offered in 2021 at Innopolis University.'
 
 
 @app.route('/generate')
