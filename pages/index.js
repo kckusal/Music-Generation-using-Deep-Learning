3077c22dbfcc2f3fa8498af8034b703b15067d08
Kusal KC <kckusal99@gmail.com>
2021-03-06 16:19:21 +0300

    add playing of generated song

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import Head from "next/head";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import styles from "../styles/Home.module.css";
 
 const GENERATE_API_URL =
   "https://music-generation-using-deep-learning.vercel.app/generate";
 
 export default function Home() {
+  const instrumentRef = useRef();
+
   const [isGenerating, setIsGenerating] = useState(false);
   const [result, setResult] = useState();
   const [error, setError] = useState();
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    instrumentRef.current = Instrument;
+  }, []);
 
   const generate = useCallback(() => {
     setIsGenerating(true);
@@ -24,10 +32,30 @@ export default function Home() {
       .finally(() => setIsGenerating(false));
   }, []);
 
+  const play = useCallback(() => {
+    if (!instrumentRef.current) {
+      alert("Cannot find Instrument reference from musical.js library.");
+      return;
+    }
+
+    if (!result) {
+      alert("No ABC text found to play!");
+      return;
+    }
+
+    const inst = new instrumentRef.current();
+    setIsPlaying(true);
+    inst.play(result, () => {
+      setIsPlaying(false);
+    });
+  }, [result, instrumentRef.current]);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Music Generation Using Deep Learning</title>
+
+        <script type="text/javascript" src={"scripts/musical.min.js"}></script>
       </Head>
 
       <main className={styles.main}>
@@ -44,13 +72,25 @@ export default function Home() {
           {isGenerating
             ? "Generating..."
             : result
-            ? ""
+            ? "The following ABC text was generated:"
             : "You have NOT generated any music yet."}
         </p>
+        <pre>
+          {!isGenerating &&
+            (error ? (
+              <em>Something went wrong! Try again.</em>
+            ) : (
+              <code>{result}</code>
+            ))}
+        </pre>
+
         <p>
           &nbsp;
-          {!isGenerating &&
-            (error ? <em>Something went wrong! Try again.</em> : result)}
+          {result && (
+            <button disabled={isPlaying} onClick={play}>
+              Play
+            </button>
+          )}
         </p>
       </main>
     </div>
